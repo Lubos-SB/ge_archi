cd188d7ea2d4b2021b4932173ba496f759ea8b1e
Enterprise Architect - Consultant <124317537+Lubos-SB@users.noreply.github.com>
2026-06-19 12:38:18 +0200

    Add files via upload

---
+++ tests/userSettings-Spec.js
@@ -0,0 +1,30 @@
+import * as userSettings from '../src/components/userSettings.js';
+
+describe("userSettings", function() {
+   
+    it("should get default user settings", () => {
+        const settings = userSettings.getSettings();
+        expect(settings).toEqual({
+            userLoadedModel: false,
+            userLoadedModelFilename: "",
+            stickyNodesOnDrag_Enabled: true
+        });
+    });
+    
+    it("should get default userLoadedModel user setting", () => {
+        const setting = userSettings.getSetting('userLoadedModel');
+        
+        expect(setting).toEqual(false);
+    });
+    
+    it("should update userLoadedModel user setting", () => {
+        userSettings.updateSetting('userLoadedModel', true);
+
+        const setting = userSettings.getSetting('userLoadedModel');
+
+        userSettings.updateSetting('userLoadedModel', false);
+
+        expect(setting).toEqual(true);        
+    });
+
+});
